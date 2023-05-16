4824d430c5e5c3a9ce5a7dc85d79ea0eaa3814be
Augusto.Klein <augusto_klein@hotmail.com>
2023-05-15 22:05:23 -0300

    Alteracao do projeto para WinsowsForm

--- Analyser/Analyser/Program.cs
+++ Analyser/Analyser/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analyser
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
