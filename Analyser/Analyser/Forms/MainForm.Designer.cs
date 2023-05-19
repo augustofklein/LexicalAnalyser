bd1d1497765f243f648e9bf95f0a74a5ba9c53c5
Augusto.Klein <augusto_klein@hotmail.com>
2023-05-18 22:13:13 -0300

    Ajuste do layout da tela e abertura de selecao do arquivo em C

---
+++ Analyser/Analyser/Forms/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Analyser
+{
+    partial class LexicalAnaliser
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelecionarArquivo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.linha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.coluna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lexema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelecionarArquivo
+            // 
+            this.btnSelecionarArquivo.Location = new System.Drawing.Point(360, 12);
+            this.btnSelecionarArquivo.Name = "btnSelecionarArquivo";
+            this.btnSelecionarArquivo.Size = new System.Drawing.Size(137, 20);
+            this.btnSelecionarArquivo.TabIndex = 10;
+            this.btnSelecionarArquivo.Text = "Selecionar Arquivo";
+            this.btnSelecionarArquivo.UseVisualStyleBackColor = true;
+            this.btnSelecionarArquivo.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.linha,
+            this.coluna,
+            this.token,
+            this.lexema});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(485, 384);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // linha
+            // 
+            this.linha.HeaderText = "Linha";
+            this.linha.Name = "linha";
+            // 
+            // coluna
+            // 
+            this.coluna.HeaderText = "Coluna";
+            this.coluna.Name = "coluna";
+            // 
+            // token
+            // 
+            this.token.HeaderText = "Token";
+            this.token.Name = "token";
+            // 
+            // lexema
+            // 
+            this.lexema.HeaderText = "Lexema";
+            this.lexema.Name = "lexema";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "c";
+            this.openFileDialog.FileName = "openFD";
+            this.openFileDialog.InitialDirectory = "@C:\\";
+            this.openFileDialog.Title = "Localizar Arquivo";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(342, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // LexicalAnaliser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 424);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSelecionarArquivo);
+            this.Name = "LexicalAnaliser";
+            this.Text = "Analisador Lêxido C";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSelecionarArquivo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn linha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn coluna;
+        private System.Windows.Forms.DataGridViewTextBoxColumn token;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lexema;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Analyser/Analyser/Forms/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Analyser
+{
+    public partial class LexicalAnaliser : Form
+    {
+        public LexicalAnaliser()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult dr = this.openFileDialog.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+    }
+}
